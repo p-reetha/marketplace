a426779d85cf438534225b213dd5da37b4e9a912
Preetha <preetha1999chennai@gmail.com>
2020-08-15 02:11:59 -0700

    file consists of routes used for this project

--- shop/routes.py
+++ shop/routes.py
@@ -1,5 +1,5 @@
 from shop import app
-from flask import request, render_template, redirect, url_for, flash
+from flask import request, render_template, redirect, url_for, flash, session
 from shop.methods import*
 
 
@@ -30,8 +30,10 @@ def login():
     if request.method == 'POST':
         mail_id = request.form.get('mail_id')
         password = request.form.get('password')
-        log_in = is_user_exists(mail_id, password)
+        log_in, user_id, user_name = is_user_exists(mail_id, password)
         if log_in == 'true':
+            session['buyer_id'] = user_id
+            session['buyer_name'] = user_name
             return redirect(url_for('categories'))
         else:
             flash("Incorrect mail-id/password", "danger")
@@ -47,57 +49,73 @@ def logout():
 @app.route('/categories', methods=['GET'])
 def categories():
     categories_list = get_categories_list()
-    return categories_list
+    return str(categories_list)
 
 
 @app.route('/products', methods=['GET'])
 def products():
     category = request.form['category']
     list_of_products = get_products(category)
-    return list_of_products
+    return str(list_of_products)
 
 
 @app.route('/cart', methods=['POST'])
 def add_to_cart():
-    prod_id = request.form['prod_id'] # need to pass hidden value of prod_id in UI
-    quantity = request.form['quantity']
-    is_product_added = add_product_to_cart(prod_id, quantity)
-    if is_product_added == 'true':
-        return redirect(url_for('categories'))
-    else:
-        return redirect(url_for('products'))
+    if request.method == 'POST':
+        prod_id = request.form['prod_id'] # need to pass hidden value of prod_id in UI
+        desired_quantity = request.form['desired_quantity']
+        buyer_id = session['buyer_id']
+        is_product_added = add_product_to_cart(prod_id, desired_quantity, buyer_id)
+        if is_product_added == 'true':
+            return redirect(url_for('categories'))
+        else:
+            return is_product_added
 
 
 @app.route('/cart', methods=['GET'])
 def view_cart():
-    products_list = products_in_cart()
-    if products_list != 0:
-        return products
-    else:
-        return 'cart is empty'
+    if request.method == 'GET':
+        buyer_id = session['buyer_id']
+        products_list = products_in_cart(buyer_id)
+        if products_list != 0:
+            return products_list
+        else:
+            return 'cart is empty'
 
 
 @app.route('/cart', methods=['DELETE'])
 def remove_from_cart():
-    cart_item_id = request.form['cart_item_id']
-    is_product_deleted = remove_product_from_cart(cart_item_id)
-    if is_product_deleted == 'true':
-        return redirect(url_for('cart.html'))
+    if request.method == 'DELETE':
+        cart_item_id = request.form['cart_item_id']
+        buyer_id = session['buyer_id']
+        is_product_deleted = remove_product_from_cart(cart_item_id, buyer_id)
+        if is_product_deleted == 'true':
+            return render_template('cart.html')
 
 
 @app.route('/cart', methods=['PUT'])
 def update_cart():
-    quantity = request.form['quantity']
-    cart_item_id = request.form['cart_item_id']
-    is_quantity_updated = update_cart_product_quantity(cart_item_id, quantity)
-    if is_quantity_updated == 'true':
-        return redirect(url_for('cart.html'))
+    if request.method == 'PUT':
+        cart_item_id = request.form['cart_item_id']
+        desired_quantity = request.form['desired_quantity']
+        buyer_id = session['buyer_id']
+        is_quantity_updated = update_cart_product_quantity(cart_item_id, desired_quantity, buyer_id)
+        if is_quantity_updated == 'true':
+            return render_template('cart.html')
+        else:
+            return is_quantity_updated
 
 
 @app.route('/buy', methods=['PUT'])
 def buy_product():
-    cart_item_id = request.form['cart_item_id']
-    is_product_purchased = product_to_buy(cart_item_id)
-    if is_product_purchased == 'true':
-        return redirect(url_for('categories'))
+    if request.method == 'PUT':
+        cart_item_id = request.form['cart_item_id']
+        desired_quantity = request.form['desired_quantity']
+        buyer_id = session['buyer_id']
+        is_product_purchased = product_to_buy(cart_item_id, desired_quantity, buyer_id)
+        if is_product_purchased == 'true':
+            return redirect(url_for('categories'))
+        else:
+            return is_product_purchased
+
 
