250e95f02348d3ae277a62d8d503b8b3d4e360e1
Preetha <preetha1999chennai@gmail.com>
2020-08-24 16:04:48 +0530

    validation mathods has been added here

--- shop/buyer_model.py
+++ shop/buyer_model.py
@@ -1,3 +1,4 @@
+import re
 from shop.db_connection import connect_db
 from sqlalchemy import Integer, String, Column
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,6 +14,34 @@ class Buyer(Base):
     mail_id = Column(String(120), unique=True, nullable=False)
     phone_no = Column(String(15), unique=True, nullable=False)
     password = Column(String(20), unique=True, nullable=False)
+
+
+def validate_name(name):
+    if re.match(r'^[a-zA-Z]+$', name):
+        return 'true'
+    else:
+        return 'false'
+
+
+def validate_mail_id(mail_id):
+    if re.match(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', mail_id):
+        return 'true'
+    else:
+        return 'false'
+
+
+def validate_phone_no(phone_no):
+    if re.match(r'^\d{10}$', phone_no):
+        return 'true'
+    else:
+        return 'false'
+
+
+def confirm_password(password, confirm):
+    if password == confirm:
+        return 'true'
+    else:
+        return 'false'
 
 
 def save_user(buyer_name, mail_id, phone_no, password):
