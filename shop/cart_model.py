362ea63e85c3c1f5de07713de4d7956dc960c41a
Preetha <preetha1999chennai@gmail.com>
2020-08-25 11:12:24 +0530

    two methods has been included to buy items in cart

--- shop/cart_model.py
+++ shop/cart_model.py
@@ -1,9 +1,12 @@
 from shop.db_connection import connect_db
+from shop.buyer_model import Buyer
 from shop.product_model import Product
 from sqlalchemy import Integer, Column, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 products_in_cart = []
+products_availability_list = []
+purchased_products = []
 
 db = connect_db()
 
@@ -11,8 +14,8 @@ db = connect_db()
 class Cart(Base):
     __tablename__ = 'carts'
     cart_item_id = Column(Integer, primary_key=True)
-    buyer_id = Column(Integer, ForeignKey('buyers.buyer_id'))
-    prod_id = Column(Integer, ForeignKey('products.prod_id'))
+    buyer_id = Column(Integer, ForeignKey(Buyer.buyer_id))
+    prod_id = Column(Integer, ForeignKey(Product.prod_id)) # 'products.prod_id'
     desired_quantity = Column(Integer, unique=False, nullable=False)
 
 
@@ -58,20 +61,37 @@ def update_cart_product_quantity(prod_id, desired_quantity, buyer_id):
     return 'true'
 
 
-def product_to_buy(prod_id, desired_quantity, buyer_id):
-    cart = db.query(Cart).filter_by(buyer_id=buyer_id, prod_id=prod_id).one()
-    product = db.query(Product).filter_by(prod_id=prod_id).one()
-    if product.prod_quantity == 0:
-        product.prod_availability = 'no'
-        db.add(product)
-        db.commit()
-        return 'stock is not available'
-    elif product.prod_quantity < desired_quantity:
-        return str(product.prod_quantity) + ' items only available'
-    else:
-        product.prod_quantity -= int(desired_quantity)
+def cart_products_availability(cart_products):
+    for cart_product in cart_products:
+        product = db.query(Product).filter_by(prod_id=cart_product['prod_id']).one()
         if product.prod_quantity == 0:
-            product.prod_quantity = 'no'
-        db.add(product)
-        db.commit()
-        return 'true'
+            product_availability_dict = {
+                "product_name": product.prod_name,
+                "availability": "Not available"
+            }
+        elif product.prod_quantity < cart_product['desired_quantity']:
+            product_availability_dict = {
+                "product_name": product.prod_name,
+                "availability": str(product.prod_quantity) + ' items only available'
+            }
+        else:
+            product_availability_dict = {
+                "product_name": product.prod_name,
+                "availability": "Available"
+            }
+        products_availability_list.append(product_availability_dict)
+    return products_availability_list
+
+
+def get_products_to_buy(cart_products, buyer_id):
+    for cart_product in cart_products:
+        product = db.query(Product).filter_by(prod_id=cart_product['prod_id']).one()
+        if product.prod_quantity > cart_product['desired_quantity']:
+            product.prod_quantity -= cart_product['desired_quantity']
+            remove_product_from_cart(cart_product['prod_id'], buyer_id)
+            if product.prod_quantity == 0:
+                product.prod_availability = 'no'
+            db.add(product)
+            db.commit()
+            purchased_products.append(cart_product['prod_name'] + ' - ' + cart_product['category'])
+    return purchased_products
